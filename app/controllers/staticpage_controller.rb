cb7b9bfd7c12843733503b82dc01515698024dc8
samsolomon <samuelrobertsolomon@gmail.com>
2012-10-17 15:15:12 -0500

    removed semicolons from staticpage_controller.rb file

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -3,11 +3,11 @@ class StaticpageController < ApplicationController
   def home
     startup_array = IO.readlines("app/assets/startuplist.txt")
     dice = rand(startup_array.length)
-    @startup = startup_array[dice];
+    @startup = startup_array[dice]
 
     occupation_array = IO.readlines("app/assets/occupationlist.txt")
     dice = rand(occupation_array.length)
-    @occupation = occupation_array[dice];
+    @occupation = occupation_array[dice]
   end
 
 end
